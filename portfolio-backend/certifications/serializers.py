23a887424f39cf40a825f59855f5c98287db5b82
AbuArwa001 <khalfanathman12@yahoo.com>
2026-02-02 23:43:55 +0300

    seperate concern

--- portfolio-backend/certifications/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Certification
-
-class CertificationSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.id')
-    class Meta:
-        model = Certification
-        fields = "__all__"
