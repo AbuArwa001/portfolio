0b767a99eac359eca56d6ae7bb30dc6aa79022e9
AbuArwa001 <khalfanathman12@yahoo.com>
2026-09-23 03:38:12 +0300

    feat: add AWS S3 hostnames to remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -48,6 +48,22 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "portfolikhalif.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "portfolikhalif.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.*.amazonaws.com",
+      },
+      {
+        protocol: "https",
         hostname: "raw.githubusercontent.com",
       },
     ],
